23e239b64fa0f7d8f15b8a148aae5858bf4cd7c3
Hoseyn <hoseyn.mousavi78@gmail.com>
2021-12-02 11:26:10 +0330

    fix

--- src/helpers/createMaterialColor.js
+++ src/helpers/createMaterialColor.js
@@ -1,8 +1,16 @@
 import hexToRgba from "./hexToRgba"
+import getComputedStyleHelper from "./getComputedStyleHelper"
 
-function createMaterialColor({variable, alpha = "0.3"})
+function createMaterialColor({variable, alpha = 0.3})
 {
-    return hexToRgba(getComputedStyle(document.documentElement).getPropertyValue(variable), alpha)
+    const color = getComputedStyleHelper(variable)
+    if (color.includes("rgba"))
+    {
+        const split = color.split(",")
+        const a = split[3]
+        return color.replace(a, alpha + ")")
+    }
+    else return hexToRgba(color, alpha)
 }
 
 export default createMaterialColor
