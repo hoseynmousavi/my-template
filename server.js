28d9dd479edfe2ef8b6ba6f33968ff4343a62078
Hoseyn <hoseyn.mousavi78@gmail.com>
2021-12-24 13:27:02 +0330

    added ducker

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.route("/static/:folder/:file").get((req, res) =>
     if (fs.existsSync(path.join(__dirname, `/build/static/${req.params.folder}/${req.params.file}`)))
     {
         res.setHeader("Vary", "Accept-Encoding")
-        res.setHeader("Cache-Control", "max-age=2592000")
+        res.setHeader("Cache-Control", "max-age=2592000, public")
         res.sendFile(path.join(__dirname, `/build/static/${req.params.folder}/${req.params.file}`))
     }
     else res.sendStatus(404)
@@ -25,7 +25,7 @@ app.route("/:file").get((req, res) =>
     if (fs.existsSync(path.join(__dirname, `/build/${req.params.file}`)))
     {
         if (req.params.file === "service-worker.js" || req.params.file === "asset-manifest.json") res.setHeader("Cache-Control", "no-store, max-age=0")
-        else res.setHeader("Cache-Control", "max-age=604800")
+        else res.setHeader("Cache-Control", "max-age=604800, public")
         res.sendFile(path.join(__dirname, `/build/${req.params.file}`))
     }
     else
@@ -39,7 +39,8 @@ app.route("*").get((req, res) =>
 {
     res.setHeader("Vary", "Accept-Encoding")
     res.setHeader("Cache-Control", "no-store, max-age=0")
-    res.sendFile(path.join(__dirname, "/build/index.html"))
+    if (fs.existsSync(path.join(__dirname, "/build/index.html"))) res.sendFile(path.join(__dirname, "/build/index.html"))
+    else res.sendFile(path.join(__dirname, "/updating.html"))
 })
 
 app.listen(process.env.REACT_APP_PORT, () => console.log(`Server is running in ${process.env.REACT_APP_PORT} ... `))
