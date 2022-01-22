cfdf6d553a8dc61b73f25f90d39c17fc3215e2b2
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-01-22 14:20:58 +0330

    f

--- src/helpers/changeBodyOverflow.js
+++ src/helpers/changeBodyOverflow.js
@@ -1,37 +1,27 @@
-import isIos from "./isIos"
-import isStandalone from "./isStandalone"
 import checkViewPort from "./checkViewPort"
+
+let isHide = false
 
 function changeBodyOverflow(makeHide)
 {
     if (makeHide)
     {
-        if (isIos() && isStandalone()) lockScroll()
-        else if (checkViewPort()) document.getElementById("root").style.overflowY = "hidden"
-        else document.body.style.overflowY = "hidden"
+        if (!isHide)
+        {
+            isHide = true
+            if (checkViewPort()) document.getElementById("root").style.overflowY = "hidden"
+            else document.body.style.overflowY = "hidden"
+        }
     }
     else
     {
-        if (isIos() && isStandalone()) unlockScroll()
-        else if (checkViewPort()) document.getElementById("root").style.removeProperty("overflow-y")
-        else document.body.style.removeProperty("overflow-y")
+        if (isHide)
+        {
+            isHide = false
+            if (checkViewPort()) document.getElementById("root").style.removeProperty("overflow-y")
+            else document.body.style.removeProperty("overflow-y")
+        }
     }
-}
-
-function unlockScroll()
-{
-    document.body.style.removeProperty("position")
-    document.body.style.removeProperty("top")
-    document.body.style.removeProperty("left")
-    document.body.style.removeProperty("right")
-}
-
-function lockScroll()
-{
-    document.body.style.position = "fixed"
-    document.body.style.top = "0"
-    document.body.style.left = "0"
-    document.body.style.right = "0"
 }
 
 export default changeBodyOverflow
