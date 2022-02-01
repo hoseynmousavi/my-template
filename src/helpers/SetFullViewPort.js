627e5fe78e9c9254452cdeaf13bdd3351c0b156a
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-02-01 13:53:19 +0330

    fx

--- src/helpers/SetFullViewPort.js
+++ src/helpers/SetFullViewPort.js
@@ -1,24 +1,18 @@
 import Resize from "./Resize"
-import {useRef} from "react"
 
 function SetFullViewPort()
 {
     const {clientWidth, clientHeight} = Resize()
-    const timer = useRef(null)
     const viewport = +process.env.REACT_APP_DESKTOP_VIEWPORT.replace("px", "")
 
-    clearTimeout(timer.current)
-    timer.current = setTimeout(() =>
-    {
-        document.documentElement.style.setProperty(
-            "--full-viewport",
-            clientWidth > viewport ? viewport + "px" : "100vw",
-        )
-        document.documentElement.style.setProperty(
-            "--full-height",
-            clientHeight + "px",
-        )
-    }, 10)
+    document.documentElement.style.setProperty(
+        "--full-viewport",
+        clientWidth > viewport ? viewport + "px" : "100vw",
+    )
+    document.documentElement.style.setProperty(
+        "--full-height",
+        clientHeight + "px",
+    )
 }
 
 export default SetFullViewPort
