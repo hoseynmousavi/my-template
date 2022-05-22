91ea0d213ae4b2dcbaa37c3df722da16afd80d35
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-05-22 15:00:53 +0430

    fx

--- src/views/components/ImageLoading.js
+++ src/views/components/ImageLoading.js
@@ -1,54 +1,20 @@
-import {forwardRef, useLayoutEffect, useState} from "react"
-import ImageLoaderWorker from "../../workers/imageLoaderWorker"
-import firstView from "../../helpers/firstView"
+import {forwardRef, useRef} from "react"
 
 const ImageLoading = forwardRef(({className, style, src, alt, loading, onClick, draggable}, ref) =>
 {
-    const [isLoaded, setIsLoaded] = useState("")
+    const contRef = useRef(null)
 
-    useLayoutEffect(() =>
+    function loaded()
     {
-        let onMessage
-        if (loading === "lazy") firstView({ref, callback: () => onMessage = loadImage()})
-        else onMessage = loadImage()
-
-        return () => onMessage && ImageLoaderWorker.removeEventListener("message", onMessage)
-        // eslint-disable-next-line
-    }, [])
-
-    function loadImage()
-    {
-        function onMessage(event)
-        {
-            const {imageURL, blob} = event.data
-            if (src === imageURL)
-            {
-                const objectURL = URL.createObjectURL(blob)
-                setIsLoaded(objectURL)
-                if (ref?.current) ref.current.onload = () => URL.revokeObjectURL(objectURL)
-            }
-        }
-
-        if (src)
-        {
-            if (src.includes("http") && window.Worker)
-            {
-                ImageLoaderWorker.postMessage(src)
-                ImageLoaderWorker.addEventListener("message", onMessage)
-                return onMessage
-            }
-            else
-            {
-                const img = new Image()
-                img.src = src
-                if (!img.complete) img.onload = () => setIsLoaded(src)
-                else if (img.complete && !isLoaded) setIsLoaded(src)
-            }
-        }
+        contRef.current.className = className
+        ref.current.className = `${className} image-loading-main loaded`
     }
 
-    if (!isLoaded) return <div className={`${className} image-loading-not-loaded`} style={style} ref={ref}/>
-    else return <img draggable={draggable} className={`${className} image-loading-loaded`} style={style} ref={ref} src={isLoaded} alt={alt} loading={loading} onClick={onClick}/>
+    return (
+        <div className={`${className} image-loading`} style={style} ref={contRef}>
+            <img draggable={draggable} className={`${className} image-loading-main`} onLoad={loaded} style={style} ref={ref} src={src} alt={alt} loading={loading} onClick={onClick}/>
+        </div>
+    )
 })
 
 export default ImageLoading
--- src/workers/workerBuilder.js
+++
@@ -1,8 +0,0 @@
-function workerBuilder(worker)
-{
-    const code = worker.toString()
-    const blob = new Blob([`(${code})()`])
-    return new Worker(URL.createObjectURL(blob))
-}
-
-export default workerBuilder
