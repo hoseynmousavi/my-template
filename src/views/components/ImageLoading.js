5eb1874f36943c8821e23c1cf92ad4b531950811
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-05-23 18:16:41 +0430

    fx

--- src/views/components/ImageLoading.js
+++ src/views/components/ImageLoading.js
@@ -6,14 +6,13 @@ const ImageLoading = forwardRef(({className, style, src, alt, loading, onClick, 
 
     function loaded()
     {
-        contRef.current.removeAttribute("class")
-        contRef.current.style.display = "contents"
-        ref.current.className = `image-loading-main loaded ${className}`
+        contRef.current.className = className
+        ref.current.className = `${className} image-loading-main loaded`
     }
 
     return (
         <div className={`${className} image-loading`} style={style} ref={contRef}>
-            <img draggable={draggable} className="image-loading-main" onLoad={loaded} style={style} ref={ref} src={src} alt={alt} loading={loading} onClick={onClick}/>
+            <img draggable={draggable} className={`${className} image-loading-main`} onLoad={loaded} style={style} ref={ref} src={src} alt={alt} loading={loading} onClick={onClick}/>
         </div>
     )
 })
