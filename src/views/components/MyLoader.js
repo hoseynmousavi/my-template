34e7e6df6c3d45868ecf98582530f20529b5a59f
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-06-14 13:47:16 +0430

    fx

--- src/views/components/MyLoader.js
+++ src/views/components/MyLoader.js
@@ -1,14 +1,14 @@
 import MyLoaderIos from "./MyLoaderIos"
 import checkOs from "../../helpers/checkOs"
 
-function MyLoader({className, width = 40, strokeWidth = 3})
+function MyLoader({className, width = 40, strokeWidth = 3, color})
 {
-    if (checkOs() === "mac" || checkOs() === "ios") return <MyLoaderIos className={className} width={width}/>
+    if (checkOs() === "mac" || checkOs() === "ios") return <MyLoaderIos className={className} width={width} color={color}/>
     else
     {
         return (
             <svg className={`circular ${className}`} width={width} height={width} viewBox="25 25 50 50">
-                <circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth={strokeWidth} strokeMiterlimit="10"/>
+                <circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth={strokeWidth} strokeMiterlimit="10" style={color && {stroke: color}}/>
             </svg>
         )
     }
--- src/views/components/MyLoaderIos.js
+++ src/views/components/MyLoaderIos.js
@@ -1,6 +1,6 @@
 import {useLayoutEffect, useRef} from "react"
 
-function MyLoaderIos({className, width})
+function MyLoaderIos({className, width, color})
 {
     const contRef = useRef(null)
     const timer = useRef(null)
@@ -23,14 +23,30 @@ function MyLoaderIos({className, width})
 
     return (
         <div className={`ios-loader ${className}`} ref={contRef} style={{width: width + "px", height: width + "px"}}>
-            <div className="ios-loader-part one"/>
-            <div className="ios-loader-part two"/>
-            <div className="ios-loader-part three"/>
-            <div className="ios-loader-part four"/>
-            <div className="ios-loader-part five"/>
-            <div className="ios-loader-part six"/>
-            <div className="ios-loader-part seven"/>
-            <div className="ios-loader-part eight"/>
+            <div className="ios-loader-part one">
+                <div className="ios-loader-part-after" style={color && {backgroundColor: color}}/>
+            </div>
+            <div className="ios-loader-part two">
+                <div className="ios-loader-part-after" style={color && {backgroundColor: color}}/>
+            </div>
+            <div className="ios-loader-part three">
+                <div className="ios-loader-part-after" style={color && {backgroundColor: color}}/>
+            </div>
+            <div className="ios-loader-part four">
+                <div className="ios-loader-part-after" style={color && {backgroundColor: color}}/>
+            </div>
+            <div className="ios-loader-part five">
+                <div className="ios-loader-part-after" style={color && {backgroundColor: color}}/>
+            </div>
+            <div className="ios-loader-part six">
+                <div className="ios-loader-part-after" style={color && {backgroundColor: color}}/>
+            </div>
+            <div className="ios-loader-part seven">
+                <div className="ios-loader-part-after" style={color && {backgroundColor: color}}/>
+            </div>
+            <div className="ios-loader-part eight">
+                <div className="ios-loader-part-after" style={color && {backgroundColor: color}}/>
+            </div>
         </div>
     )
 }
